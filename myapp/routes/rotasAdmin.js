5ebf4dc6e9c46184f082c4791d41a80348ed42c6
felipekian <felipekian@yahoo.com.br>
2019-05-20 20:20:27 -0400

    Feito as interfaces parciais da sessão de administração

--- myapp/routes/rotasAdmin.js
+++ myapp/routes/rotasAdmin.js
@@ -3,8 +3,33 @@ var router = express.Router();
 
 /* GET's */
 router.get('/', function(req, res, next) {
-  res.render('./loja_admin/index', { title: 'Rotas admin' });
+  res.render('./loja_admin/index');
 });
+
+router.get('/admincadastro', function(req, res, next) {
+    res.render('./loja_admin/cadastro');
+});
+
+router.get('/adminrecuperar', function(req, res, next) {
+    res.render('./loja_admin/recuperar');
+});
+
+router.get('/home', function(req, res, next) {
+    res.render('./loja_admin/home');
+});
+
+router.get('/cadastro_produto', function(req, res, next) {
+    res.render('./loja_admin/cadastro_produtos');
+});
+
+router.get('/clientes', function(req, res, next) {
+    res.render('./loja_admin/clientes');
+});
+
+router.get('/chat', function(req, res, next) {
+    res.render('./loja_admin/chat_cliente');
+});
+
 
 
 /* POST's */
